9785710dd13428f23a813cc79c7b907020a147f5
sebasguti0607 <sebastiangutierrez0607@gmail.com>
2024-09-08 18:02:01 -0600

    Inicio de las primeras interfaces (view) y de los primeros modelos (model)

---
+++ Practica1/src/model/Lista_Productos.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+
+ @author sebas
+ */
+
+//Clase modelo que contiene la 
+public class Lista_Productos {
+    public static List<Producto> productos = new ArrayList<>();
+}
---
+++ Practica1/src/model/Producto.java
@@ -0,0 +1,48 @@
+package model;
+
+/**
+
+ @author sebas
+ */
+public class Producto {
+    
+    private String codigo;
+    private String nombre;
+    private String material;
+    private String color;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    
+    
+}
